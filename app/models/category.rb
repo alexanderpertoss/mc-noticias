3ca0f6e7cfb16799ab235b80fcd38d42e70f41cf
Alexander Peredo Torossova <alexgunner2012@gmail.com>
2025-03-27 15:47:36 -0400

    change to not display categories without an article in the index page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
 	has_many :articles
-
 	validates :name, presence: true
+	after_initialize :set_defaults
 
 	def self.available_categories
     	# excluding the "ultimo momento" category with id=8 and multimedia with id=3 and "gente que hace noticia" with id=4
@@ -14,6 +14,17 @@ class Category < ApplicationRecord
     	# excluding the "ultimo momento" category with id=8 and multimedia with id=3 and "gente que hace noticia" with id=4
     	excluded_category_ids = [8, 3, 4]  
 
-   		categories = where.not(id: excluded_category_ids).order(queue_position: :desc).offset(3)
+   		#categories = where.not(id: excluded_category_ids).order(queue_position: :desc).offset(3)
+   		categories = where.not(id: excluded_category_ids)
+		  .joins(:articles)
+		  .group('categories.id')
+		  .having('COUNT(articles.id) > 0')
+		  .order(queue_position: :desc)
+		  .offset(3)
+  	end
+
+  	private
+  	def set_defaults
+  	  self.queue_position ||= 0
   	end
 end
