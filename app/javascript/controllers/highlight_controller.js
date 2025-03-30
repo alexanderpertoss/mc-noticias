d70bcaffb20032aff99fbba65ed74f866f6011df
Alexander Peredo Torossova <alexgunner2012@gmail.com>
2025-03-30 11:10:16 -0400

    slowed down the speed of breaking news

--- app/javascript/controllers/highlight_controller.js
+++ app/javascript/controllers/highlight_controller.js
@@ -4,6 +4,6 @@ export default class extends Controller {
   connect() {
     setTimeout(() => {
       this.element.classList.remove("highlight");
-    }, 3000); // Remove highlight after 1 second
+    }, 3000);
   }
 }
